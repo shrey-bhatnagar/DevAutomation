487d09b38a786b9a91822b161c2ce271edefad76
Aabid-Hussain <aabid.hussain@info.com>
2017-12-27 15:07:36 +0530

    changed class name of applogging.py.

--- main.py
+++ main.py
@@ -8,18 +8,16 @@ import time
 import pdb
 import logging
 from Script_Dir.sshAutomation import Devstack
-from Script_Dir.osloconfig import get_config
-from Script_Dir.osloconfig import shrey
 from Script_Dir.readjson import get_jsonconfig
-from Script_Dir.applogging import Logger_check
+from Script_Dir.applogging import LoggerCreate
 
 
-mylog = Logger_check('main', logging.INFO)
+my_Log = LoggerCreate('event_logger', logging.INFO)
 
 
-def myprint(str):
+def print_log(str):
     print(str)
-    mylog.log.info(str)
+    my_Log.log.info(str)
 
 
 def maincompilationcheck():
@@ -36,7 +34,7 @@ def deluser(command, delusername):
     command.cmd(cmd1)
 
 
-def Check_test_status(list_data, last_val):
+def check_test_status(list_data, last_val):
     data1, newlist = [], []
     count_pass = 0
     count_fail = 0
@@ -51,26 +49,31 @@ def Check_test_status(list_data, last_val):
             count_pass = count_pass + 1
         elif 'FAIL' in s:
             count_fail = count_fail + 1
-    print("Cases PASS == %s" % count_pass)
-    print("Cases FAIL == %s" % count_fail)
+    print_log("TEST execution Completed")
+    print_log ( ":::Summary:::")
+    print_log("Cases PASS == %s" % count_pass)
+    print_log("Cases FAIL == %s" % count_fail)
 
 
-def devsetup(devinstalation, devdata):
-    apt_git_success, apt_git_error = \
-            devinstalation.cmd('apt install git -y', sudo=True)
-    if apt_git_success[3] == 'git is already the newest version.\n':
-        myprint('##########################################')
-        myprint("### Git is already the newest version. ###")
-        myprint('##########################################')
-    elif apt_git_success[3] == 'Suggested packages:\n':
-        myprint('##############################')
-        myprint("### Git has been installed ###")
-        myprint('##############################')
-    else:
-        myprint(" GIT install Has some error ... Please re-verify")
-
+def devstack_infra_creation(devinstalation, devdata):
     devVersion = \
         'git clone https://github.com/shrey-bhatnagar/DevAutomation.git'
+    apt_git_success, apt_git_error = \
+            devinstalation.cmd('apt install git -y', sudo=True)
+    print_log("Installing GIT...............")
+    success_check = 'git is already the newest version.\n'
+    fail_check = 'Suggested packages:\n'
+    if apt_git_success[3] == success_check:
+        print_log (apt_git_success)
+        print_log("Git is already the newest version....... Skipping GIT installation")
+    elif apt_git_success[3] == fail_check :
+        print_log(apt_git_error)
+        print_log(".Git has been installed successfully.")
+    else:
+        print_log(" GIT install Has some error ... Please re-verify")
+
+    print_log("Cloning repository.")
+    print_log(devVersion)
     clone_repo_success, clone_repo_fail = \
         devinstalation.cmd(devVersion, sudo=False)
     # devVersion =
@@ -80,63 +83,61 @@ def devsetup(devinstalation, devdata):
     if clone_repo_fail != []:
         sub_clone = clone_repo_fail[0].split()
         if sub_clone[0] == "Cloning":
-            myprint('#################################')
-            myprint('########Repository Cloned########')
-            myprint('#################################')
+            print_log(clone_repo_fail)
+            print_log('Repository Cloned Successfully')
         else:
             if sub_clone[0] == 'fatal:':
-                myprint('#################################')
-                myprint('####Repository Already Exists####')
-                myprint('#################################')
+                print_log(clone_repo_fail)
+                print_log('Repository Already Exists.... Cloning not required')
     devinstalation.local_conf(devdata['file_name'],
                               devdata['data_local'])
 
-    print('####### RUNNING TEST #######')
+    print_log('RUNNING DEVSTACK TEST')
 
     dev_test_success, dev_test_error =\
         devinstalation.cmd('source /opt/stack/devstack/run_tests.sh')
-    Check_test_status(dev_test_success, 13)
+    check_test_status(dev_test_success, 13)
     # print(dev_test_success, dev_test_error)
     # devinstalation.cmd('cd devstack')
     # devinstalation.cmd('source /devstack/stack')
 
 
-def usercreation(userinstalation, devdata):
-    cmd1 = "useradd -s /bin/bash -d /opt/%s -m %s" % \
+def create_devstack_user(userinstalation, devdata):
+    print_log("Creating devstack user : %s" %devdata['devUser'])
+    add_user = "useradd -s /bin/bash -d /opt/%s -m %s" % \
             (devdata['devUser'], devdata['devUser'])
-    cmd2 = 'echo "%s ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers' % \
+    add_user_to_file = 'echo "%s ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers' % \
            (devdata['devUser'])
     devusername = devdata['devUser']
     devuserpwd = devdata['devUserPwd']
     setpwd = True
-    userinstalation.rootcmd(cmd1, cmd2, setpwd, devusername, devuserpwd)
+    userinstalation.rootcmd(add_user, add_user_to_file, setpwd, devusername, devuserpwd)
+    print_log("User Creation Successfull")
 
-
-def step1(jsondata):
+def fetch_data_for_user(jsondata):
     userdata = jsondata['userdata']
     devdata = jsondata['devdata']
     userinstalation = Devstack(
             userdata['ip'], userdata['username'], userdata['password'])
     # userinstalation.rootcmd1(devdata['devUser'])
-    usercreation(userinstalation, devdata)
-    userinstalation.close()
+    create_devstack_user(userinstalation, devdata)
+    userinstalation.close_host_connection()
 
 
-def step2(devdata):
+def install_devstack(devdata):
     devinstalation = Devstack(
         devdata['ip'], devdata['devUser'], devdata['devUserPwd'])
-    devsetup(devinstalation, devdata)
-    devinstalation.close()
+    devstack_infra_creation(devinstalation, devdata)
+    devinstalation.close_host_connection()
 
 
-def connect_ssh():
+def connect_to_host():
     jsondata = get_jsonconfig()
-    step1(jsondata['appdata'])
-    step2(jsondata['appdata']['devdata'])
+    fetch_data_for_user(jsondata['appdata'])
+    install_devstack(jsondata['appdata']['devdata'])
 
 
 if __name__ == '__main__':
-    mylog.log.info('Starting main.py')
-    myprint('we are testing the printing')
-    connect_ssh()
-    mylog.log.info('Stoping main.py\r\n\r\n')
+    print_log('Starting Devstack infrastructure Creation')
+    connect_to_host()
+    print_log('Execution has been completed......Please check the log under ..../logs/event_logger.log')
